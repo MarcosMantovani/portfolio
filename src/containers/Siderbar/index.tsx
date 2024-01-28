fbd700b2d18feaf6d5c66141bc1288067f33a1ef
Marcos Mantovani <marcospiderplays@gmail.com>
2024-01-28 20:22:25 -0300

    estilizando a sidebar

--- src/containers/Siderbar/index.tsx
+++ src/containers/Siderbar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SiderbarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/marcospider21.png" />
-    <Titulo fontSize={20}>Marcos Mantovani</Titulo>
+    <SiderbarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Marcos Mantovani</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        marcospider21
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev FullStack Pyton
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SiderbarContainer>
   </aside>
 )
 
